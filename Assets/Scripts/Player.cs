a97be83e430eaee4bd740c5838dd25ff70f06512
NerdyPacman <jaxsonmyrick@hotmail.com>
2018-10-05 18:23:59 -0500

    Basic Movement
    Added running and jumping and basic collision logic.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed;
+    public float jumpForce;
+    Rigidbody2D rb;
+    Vector3 refVelocity = Vector3.zero;
+    bool grounded = false;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y), ref refVelocity, 0.05f);
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+            grounded = false;
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+}
