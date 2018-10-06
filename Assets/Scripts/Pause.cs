d9cd5f83dd64e1ea47adde2f2df3166940408958
NerdyPacman <jaxsonmyrick@hotmail.com>
2018-10-06 18:29:22 -0500

    Added Menus and Camera Following
    Added a main menu with start and quit buttons and a pause menu that pauses the game with a main menu button. Used a cinemachine for the camera following.

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+    public GameObject panel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Pause"))
+        {
+            panel.SetActive(!panel.activeInHierarchy);
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+            } else
+            {
+                Time.timeScale = 1;
+            }
+
+        }
+	}
+
+    public void SetScene(int index)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadSceneAsync(index);
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetScene (int index)
+    {
+        SceneManager.LoadSceneAsync(index);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
